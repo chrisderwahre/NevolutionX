6b41492e2277dc709402795c65631b0e338a7243
Lucas Eriksson <dracc@dracc.se>
2019-04-25 22:11:30 +0200

    findXBE better output

--- findXBE.cpp
+++ findXBE.cpp
@@ -22,7 +22,7 @@ int findXBE(void* list) {
       sprintf(tmp, "%s%s\\default.xbe", path, fData.f_FileName);
       tmpFILE = fopen(tmp, "rb");
       if (tmpFILE != nullptr) {
-        outputLine("xbe found!\n");
+        outputLine("xbe found! fn: %s, p: %s\n", fData.f_FileName, tmp);
         gmi_list->push_back(gameMenuItem(fData.f_FileName, tmp));
         outputLine("File Pushed to gameMenuItem list\n");
         fclose(tmpFILE);
