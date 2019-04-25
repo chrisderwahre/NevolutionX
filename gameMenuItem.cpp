30a96a894de5d302db38021a0e699d892600053d
Lucas Eriksson <dracc@dracc.se>
2019-04-25 22:08:35 +0200

    gameMenuItem destructor

--- gameMenuItem.cpp
+++ gameMenuItem.cpp
@@ -9,6 +9,14 @@ gameMenuItem::gameMenuItem(char* text, char* p) :
 gameMenuItem::gameMenuItem(const char* text, const char* p) :
   gameMenuItem(const_cast<char*>(text), const_cast<char*>(p)) {}
 
+gameMenuItem::~gameMenuItem() {
+  if (getTexture() != nullptr) {
+    SDL_DestroyTexture(getTexture());
+    setTexture(nullptr);
+    outputLine("gameMenuItem deconstructed!\n");
+  }
+}
+
 char* gameMenuItem::getXBEPath() {
   return xbePath;
 }
