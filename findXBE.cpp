32b84b663140c44aec911634c7cb9ae3cab337cd
Lucas Eriksson <dracc@dracc.se>
2019-04-25 22:08:14 +0200

    findXBE debug output

--- findXBE.cpp
+++ findXBE.cpp
@@ -17,11 +17,14 @@ int findXBE(void* list) {
 
   while (readFolder(fHandle, &fData) == STATUS_SUCCESS) {
     if (fData.f_Attributes & FILE_ATTRIBUTE_DIRECTORY) {
+      outputLine("Directory found!\n");
       tmp[0] = '\0';
       sprintf(tmp, "%s%s\\default.xbe", path, fData.f_FileName);
       tmpFILE = fopen(tmp, "rb");
       if (tmpFILE != nullptr) {
+        outputLine("xbe found!\n");
         gmi_list->push_back(gameMenuItem(fData.f_FileName, tmp));
+        outputLine("File Pushed to gameMenuItem list\n");
         fclose(tmpFILE);
         tmpFILE = nullptr;
       }
