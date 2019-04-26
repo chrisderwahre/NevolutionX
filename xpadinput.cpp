3e65f956eec243de64c187ec4e828e8aab2e0308
Lucas Eriksson <dracc@dracc.se>
2019-04-27 00:20:56 +0200

    xpadinput: Fix compilation errors

--- xpadinput.cpp
+++ xpadinput.cpp
@@ -1,9 +1,9 @@
 #include "xpadinput.h"
 
-_Bool getAnalogKeyDown (XPAD_INPUT *pad, int requested_key) {
+bool getAnalogKeyDown (XPAD_INPUT *pad, int requested_key) {
   return pad->PressedButtons.ucAnalogButtons[requested_key];
 }
 
-_Bool getDigitalKeyDown (XPAD_INPUT *pad, int requested_key) {
+bool getDigitalKeyDown (XPAD_INPUT *pad, int requested_key) {
   return pad->PressedButtons.usDigitalButtons & requested_key;
 }
